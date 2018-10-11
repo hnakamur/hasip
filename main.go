2d077196c19b753f259bdc9febb81c0f10109ddb
Hiroaki Nakamura <hnakamur@gmail.com>
2018-10-11 10:54:02 +0900

    Make interface flag optional to search all interfaces

--- main.go
+++ main.go
@@ -9,12 +9,13 @@ import (
 )
 
 var (
-	intfName = kingpin.Flag("interface", "Name of ethernet interface.").Short('i').Required().String()
+	intfName = kingpin.Flag("interface", "Name of ethernet interface.").Short('i').String()
 	addr     = kingpin.Flag("address", "IP address.").Short('a').Required().String()
 )
 
 func main() {
 	kingpin.Parse()
+
 	ok, err := run(*intfName, *addr)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
@@ -28,12 +29,30 @@ func main() {
 }
 
 func run(intfName, addr string) (bool, error) {
+	ip := net.ParseIP(addr)
+
+	if intfName == "" {
+		intfs, err := net.Interfaces()
+		if err != nil {
+			return false, fmt.Errorf("failed to get all interfaces: %v", err)
+		}
+		for i := range intfs {
+			ok, err := hasAddr(&intfs[i], ip)
+			if err != nil {
+				return false, fmt.Errorf("failed to check having ip: %v", err)
+			}
+			if ok {
+				return true, nil
+			}
+		}
+		return false, nil
+	}
+
 	intf, err := net.InterfaceByName(intfName)
 	if err != nil {
 		return false, fmt.Errorf("interface not found: %v", err)
 	}
 
-	ip := net.ParseIP(addr)
 	ok, err := hasAddr(intf, ip)
 	if err != nil {
 		return false, fmt.Errorf("failed to check having ip: %v", err)
